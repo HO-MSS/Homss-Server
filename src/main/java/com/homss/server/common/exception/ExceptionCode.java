f750896f680e069da2384ada0b3e2b6e84634bf0
KJBig <kjgi73k@gmail.com>
2024-10-31 12:47:22 +0900

    feat : KakaoClient 생성
    - 카카오 엑세스 토큰으로 사용자의 소셜 아이디를 가져오는 KakaoClient 구현.
    - TestCode 추가.

--- src/main/java/com/homss/server/common/exception/ExceptionCode.java
+++ src/main/java/com/homss/server/common/exception/ExceptionCode.java
@@ -10,7 +10,8 @@ public enum ExceptionCode {
 
     INTERNAL_SERVER_ERROR("0001", HttpStatus.INTERNAL_SERVER_ERROR, "알 수 없는 서버에러"),
     EXPIRED_TOKEN_ERROR("0002", HttpStatus.BAD_REQUEST, "만료된 토큰"),
-    INVALIDATE_TOKEN_ERROR("0003", HttpStatus.BAD_REQUEST, "잘못된 토큰");
+    INVALIDATE_TOKEN_ERROR("0003", HttpStatus.BAD_REQUEST, "잘못된 토큰"),
+    ID_FILED_NOT_FOUND_ERROR("0004", HttpStatus.INTERNAL_SERVER_ERROR, "ID 필드를 찾지 못함");
 
     private final String code;
     private final HttpStatus httpStatus;
